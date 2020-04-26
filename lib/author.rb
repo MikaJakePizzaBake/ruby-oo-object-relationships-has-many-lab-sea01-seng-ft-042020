3628883b89fc955a66b3461164fc935f4ab4522f
Trevor Low <tlow94@uw.edu>
2020-04-26 06:07:42 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,17 +1,10 @@
 class Author
-
-    attr_accessor :name
-
-    def initialize(name)
-        @name = name
-
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  def posts
+    Post.all.select do |post|
+      post.author == self
     end
-
-    def posts
-        Post.all.select do |post|
-            post.author == self
-        end
-    end
-
-
-end
+  end
