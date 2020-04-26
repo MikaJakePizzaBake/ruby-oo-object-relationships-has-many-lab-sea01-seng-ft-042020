3cedb037023509af68051324cb4a7f3bf4d3d037
Trevor Low <tlow94@uw.edu>
2020-04-26 05:16:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,34 @@
-class Artist
-  attr_reader :name, :songs
-  @@all = []
-  def initialize (name)
-    @name = name
-    @songs = []
+require 'pry'
+
+class User
+
+  attr_reader :username
+
+  @@all = [ ]
+
+  def initialize(username)
+    @username = username
+    # @tweets = [ ] -> we are not storing collections anymore
+
     @@all << self
   end
 
+  def tweets
+    # binding.pry
+    Tweet.all.select { |tweet| tweet.user == self }
+    # coffee_dad.tweets => [ <# Tweet >, <# tweets> ]
+  end
+
+  def self.all
+    @@all
+  end
+
+  def post_tweet(message)
+    # binding.pry
+    tweet = Tweet.new(message, self)
+    # @tweets << tweet
+  end
+  # def tweets
+  #   @tweets
+  # end
 end
