efbde5d323d74a261c15d2851df5db1dd9167fac
Trevor Low <tlow94@uw.edu>
2020-04-26 05:17:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,22 @@
-class Song
-  attr_reader :song, :artist
-  @@all =[]
-  def initialize(song)
-    @song = song
-    @artist = artist
-    @@all << self
-  end
-def self.all
-  @@all
-end
+class Tweet
+  attr_reader :message, :user
 
+  @@all = [ ]
+
+  # the child will always be responsible for storing the parent information
+  def initialize(message, user)
+    @message = message
+    @user = user
+
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  private
+    def save
+      @@all << self
+    end
 end
