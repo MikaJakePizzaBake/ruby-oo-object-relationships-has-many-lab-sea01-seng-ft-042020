e057dad241e347d4482dc94a94a381b0cfb245ef
Trevor Low <tlow94@uw.edu>
2020-04-26 05:24:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,1 @@
-class Artist
-    attr_accessor :name
 
-    def initialize(name)
-        @name = name
-        @songs = []
-    end
-
-    def songs
-        Song.all.select do |song|
-            song.artist == self
-        end
-    end
-
-
-end
