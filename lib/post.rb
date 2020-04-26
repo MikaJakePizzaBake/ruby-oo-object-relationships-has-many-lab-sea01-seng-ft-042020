14a861818e5b6207fc76afa516f2e93a4e7e76c6
Trevor Low <tlow94@uw.edu>
2020-04-26 06:07:46 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,12 @@
 class Post
-    attr_accessor :title, :author
-    @@all = []
-
-    def initialize(title)
-        @title = title
-        @author = author
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
-
-
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title
+    @author = author
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
